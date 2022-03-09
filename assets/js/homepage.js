4e97fff0a22c99284730c477fd16ea91e8920896
nzwithee@gmail.com <Cartoon#01>
2022-03-09 13:51:10 -0600

    feature updates

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,5 +1,83 @@
-var getUserRepos = function(){
-    fetch("https://api.github.com/users/octocat/repos");
+
+var userFormEl =document.querySelector("#user-form");
+var nameInputEl = document.querySelector("#username");
+
+var repoContainerEl = document.querySelector("#repos-container");
+var repoSearchTerm = document.querySelector("#repo-search-term");
+
+var formSubmitHandler = function(event){
+    event.preventDefault();
+    var username = nameInputEl.value.trim();
+
+    if (username) {
+        getUserRepos(username);
+        nameInputEl.value = "";
+    }
+    else {
+        alert("Please enter a Github Username");
+    }
+}
+
+var displayRepos = function(repos, searchTerm) {
+    //check if api returned any repos
+    if (repos.length === 0){
+        repoContainerEl.textContent ="No repositories found";
+        return;
+    }
+    
+    // clear old content
+    repoContainerEl.textContent = "";
+    repoSearchTerm.textContent = searchTerm;
+
+    for (var i = 0; i <repos.length; i++) {
+        var repoName = repos[i].owner.login + "/" + repos[i].name;
+
+        var repoEl = document.createElement("div");
+        repoEl.classList = "list-item flex-row justify-space-between align-center";
+
+        var titleEl = document.createElement("span");
+        titleEl.textContent = repoName;
+
+        var statusEl = document.createElement("span");
+        statusEl.classList = "flex-row align-center";
+
+        if (repos[i].open_issues_count > 0){
+            statusEl.innerHTML = "<i class='fas fa-times status-icon icon-danger'></i>" + repos[i].open_issues_count + " issue(s)";
+        }
+        else {
+            statusEl.innerHTML = "<i class='fas fa-check-square status-icon icon-success'></i>";
+        }
+
+        repoEl.appendChild(titleEl);
+
+        repoEl.appendChild(statusEl);
+
+        repoContainerEl.appendChild(repoEl);
+    }
+    
+
+}
+
+
+var getUserRepos = function(user){
+    //format the github api URL
+    var apiUrl = "https://api.github.com/users/" + user + "/repos";
+
+    //make a request to the url
+    fetch(apiUrl)
+    .then(function(response){
+        if (response.ok) {
+            response.json().then(function(data){
+                displayRepos(data, user);
+            });
+        }
+        else {
+            alert("Error - Github User Not Found");
+        }
+    })
+    .catch(function(error){
+        alert("Unable to connect to github")
+    });
 };
 
-getUserRepos();
+userFormEl.addEventListener("submit", formSubmitHandler);
